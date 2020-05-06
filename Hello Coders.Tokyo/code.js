bb606f3df8a677e1de86bfd7517b027c9b91a1e2
Chu Nguyen Chuong <chunguyenchuong2014bg@gmail.com>
2020-05-06 13:05:09 +0700

    lession 1, second time

--- Hello Coders.Tokyo/code.js
+++ Hello Coders.Tokyo/code.js
@@ -1,13 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 9080;
 
-app.listen(port, ()=> {
-    console.log( port + ' actived !')
+const port = 9080; 
+
+app.listen(port, ()=>{
+    console.log( port + " activated");
 })
-app.get('/user', (require, response)=>{
-    response.send('<a href = "">this  user name</a>')
-})
-app.get('/', (require, response)=>{
-    response.send('<a href = "https://www.facebook.com/chuongcnbhaf180208/"> link continue </a>')
+
+app.get('/', (req, res)=>{
+    res.send('<a href="#"> facebook </a>');
 })
