550e09bf97083430e7b09935fb6b89081cfec19c
Chu Nguyen Chuong <chunguyenchuong2014bg@gmail.com>
2020-05-20 07:43:59 +0700

    up to date controller asm 2

--- Controller MVC/route/transaction.route.js
+++ Controller MVC/route/transaction.route.js
@@ -9,7 +9,9 @@ transaction.get('/:id', (req, res) => {
         return item.id.toLowerCase().indexOf(id.toLowerCase()) !== -1;
     });
     const listBorrow = db.get('transaction').value().filter((item)=>{
-        return item.UserId === req.params.id;
+        if(item.isComplete == true){
+            return item.UserId === req.params.id;
+        }
     });
     const book = db.get('books').value().map((item)=>{
         return item
@@ -22,17 +24,28 @@ transaction.get('/:id', (req, res) => {
         };
     });
     res.render('transaction/transaction',{
-        books: db.get("books").value(),
+        books: book,
         transaction : result,
-        user : arrFindUser
+        user : arrFindUser,
+        listBorrow : listBorrow
     })
 });
 
 transaction.post('/:id',(req,res)=>{
     req.body.id = shortId.generate();
+    req.body.isComplete = true;
     req.body.UserId = req.params.id;
     db.get('transaction').push(req.body).write();
     res.redirect('/transaction/'+ req.params.id);
+});
+
+transaction.get('/:id/complete', (req,res)=>{
+    const id = req.params.id;
+    db.get('transaction')
+    .find({ id: id})
+    .assign({ isComplete : false })
+    .write();
+    res.redirect('back')
 })
 
 
